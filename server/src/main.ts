94fe6c0763dc3d81578c5213c88860d13fab262d
Ifeanyichukwu Onyekwelu <ifeanyionyekwelu786@gmail.com>
2026-04-19 17:24:40 +0100

    updated the server alowed cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://expense-tracker-ten-teal.vercel.app/',
+    origin: 'https://expense-tracker-ten-teal.vercel.app',
     credentials: true,
   });
 
